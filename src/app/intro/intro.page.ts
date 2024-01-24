1208e4ed7be0a663f81d60e1d99f02b5552325a2
Andrea Vecino <andreavecino94@gmail.com>
2024-01-23 20:04:27 -0500

    Clase 23/ene/2024

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginForm: FormGroup; // declare the loginForm property
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(
+            "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$"
+          )
+        ])
+      )
+    })
+   }
+
+  ngOnInit() {
+  }
+
+  login(login_data: any){
+    console.log(login_data);
+  }
+
+}
